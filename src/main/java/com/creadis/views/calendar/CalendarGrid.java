cf2d11580d9f3cc3e96296d5956d500bb6ad3cfa
h4sski <h4sski.programming@gmail.com>
2022-10-26 18:22:51 +0200

    Working on CalendarGrid.java to get calendar shown as grid.

--- src/main/java/com/creadis/views/calendar/CalendarGrid.java
+++ src/main/java/com/creadis/views/calendar/CalendarGrid.java
@@ -2,61 +2,26 @@ package com.creadis.views.calendar;
 
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Paragraph;
-import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.TextStyle;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.Locale;
 
 public class CalendarGrid extends VerticalLayout {
-//    private Date date2 = new Date();
-//    private Date date = new Date();
-//    private Date firstMonthDay = new Date(date.getYear(), date.getMonth(), 1);
-//    private Calendar calendar = Calendar.getInstance();
-//    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-//    private SimpleDateFormat dateMonth = new SimpleDateFormat("M");
-//    private SimpleDateFormat dateDay = new SimpleDateFormat("u");
-//    private SimpleDateFormat headerDateFormat = new SimpleDateFormat("yyyy MMM");
     private int dayNumber = 1;
-    private LocalDate localDate = LocalDate.now();
-    private int dayOfWeek = localDate.getDayOfWeek().getValue();
+    private final LocalDate localDate = LocalDate.now();
+    private int firstMonthDayOfWeek = localDate.withDayOfMonth(1).getDayOfWeek().getValue();
     private int curentDay = 1;
 
     public CalendarGrid() {
         this.addClassNames("calendar", "p-0", "m-0");
         this.setWidthFull();
 
-//        add(new Span(dateFormat.format(date)));
-//        add(new Span(dateFormat.format(firstMonthDay)));
-//        add(new Span(dateDay.format(date)));
-//        add(new Span(dateDay2.format(date)));
-//        add(new Span(String.valueOf(calendar)));
-//        add(new Span(String.valueOf(calendar.YEAR)));
-//        add(new Span(String.valueOf(calendar.MONTH)));
-//        add(new Span(String.valueOf(calendar.DAY_OF_MONTH)));
-//        add(new Span(String.valueOf(calendar.getTime())));
-//        add(new Span());
-//        add(new Span(calendar));
-
-
-//        calendar.setTime(date);
-//        Integer year = calendar.get(Calendar.YEAR);
-//        Integer month = calendar.get(Calendar.MONTH);
-//        add(new H2(headerDateFormat.format(date)));
-//        add(new H2(String.valueOf(year + " - " + month)));
-
-
         add(new H2(localDate.getYear() + " - " + localDate.getMonth()));
+        int dayOfWeek = localDate.getDayOfWeek().getValue();
         add(new H2(localDate.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ENGLISH) + " = " + dayOfWeek));
-//        add(new Span("Today is " + localDate.getYear()));
-//        add(new Span("Today is " + localDate.getMonth()));
-//        add(new Span("Today is " + localDate.getDayOfMonth()));
-//        add(new Span("Today is " + localDate.getDayOfWeek()));
 
 
         addRow(true, false);
@@ -83,7 +48,7 @@ public class CalendarGrid extends VerticalLayout {
             }
         } else if (isFirstWeek) {
             for (int i = 0; i <7; i++) {
-                row.add(Integer.toString(curentDay++));
+                row.add(createCell(Integer.toString(firstMonthDayOfWeek - 7 + i)));
             }
             row.add(createCell("buttons"));
         } else {
